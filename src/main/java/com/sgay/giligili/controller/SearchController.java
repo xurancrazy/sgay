2d8795914d51d4c0c2c5b28b1193c6fe1e7c3594
xuran <380952691@qq.com>
2016-09-25 21:44:32 +0800

    change log config

--- src/main/java/com/sgay/giligili/controller/SearchController.java
+++ src/main/java/com/sgay/giligili/controller/SearchController.java
@@ -3,8 +3,8 @@ package com.sgay.giligili.controller;
 import com.sgay.giligili.entity.TorrentMap;
 import com.sgay.giligili.service.ITorrentService;
 import com.sgay.giligili.entity.TorrentDetail;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -31,7 +31,7 @@ public class SearchController {
 
 	private static final String defaultURL = "http://www.torrentkitty.me/search/";//爬虫的基础URL
 
-	private static Logger logger=LogManager.getLogger(SearchController.class.getName());
+	private static Logger logger= LoggerFactory.getLogger(SearchController.class);
 	@RequestMapping(method=RequestMethod.POST)
 	public String getResult(@RequestParam("fanhao") String fanhao)
 	{
